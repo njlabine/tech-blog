9dab975e03833c189363131dc3154d292e24aece
Nate LaBine <njlabine@gmail.com>
2022-06-13 11:59:42 -0400

    2nd push

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,34 +2,38 @@ const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
+
 // Prevent non logged in users from viewing the homepage
-router.get("/", withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
-    });
+// router.get("/", async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ["password"] },
+//       order: [["name", "ASC"]],
+//     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+//     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render("homepage", {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render("homepage", {
+//       users,
+//       // Pass the logged in flag to the template
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-router.get("/login", (req, res) => {
-  // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
-  }
+// router.get("/login", (req, res) => {
+//   // If a session exists, redirect the request to the homepage
+//   if (req.session.logged_in) {
+//     res.redirect("/");
+//     return;
+//   }
 
-  res.render("login");
-});
+//   res.render("login");
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
